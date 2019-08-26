3bae80989b6c85d63f788c31ac05cce65890a6ef
Md Nazrul Islam <connect2nazrul@gmail.com>
2019-08-26 16:41:23 +0600

    first coverage for test setup

--- src/collective/fhirpath/testing.py
+++ src/collective/fhirpath/testing.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
-from plone.testing import z2
 
 import collective.fhirpath
 
@@ -38,14 +36,4 @@ COLLECTIVE_FHIRPATH_INTEGRATION_TESTING = IntegrationTesting(
 COLLECTIVE_FHIRPATH_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(COLLECTIVE_FHIRPATH_FIXTURE,),
     name='CollectiveFhirpathLayer:FunctionalTesting',
-)
-
-
-COLLECTIVE_FHIRPATH_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        COLLECTIVE_FHIRPATH_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name='CollectiveFhirpathLayer:AcceptanceTesting',
 )
